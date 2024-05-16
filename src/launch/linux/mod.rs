684e6f4253a7bdf447e860f8960bbad7d9fb8602
Tyler Fanelli <tfanelli@redhat.com>
2024-05-16 15:29:18 -0400

    launch/linux: Remove InitVcpu ID
    As this crate does not yet initialize a vCPU with TDX parameters,
    CmdId::InitVcpu is unused.
    
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

--- src/launch/linux/mod.rs
+++ src/launch/linux/mod.rs
@@ -7,7 +7,6 @@ pub const NR_CPUID_CONFIGS: usize = 12;
 pub enum CmdId {
     GetCapabilities = 0,
     InitVm = 1,
-    InitVcpu = 2,
 }
 
 /// Contains information for the sub-ioctl() command to be run. This is
