e84bc2741e0ae95cacc7364a5ca36d3cf07867ee
Jake Correnti <jakecorrenti+github@proton.me>
2024-05-17 14:54:22 -0400

    Enable MAX_VCPUS and SPLIT_IRQCHIP caps and create VCPU
    Signed-off-by: Jake Correnti <jakecorrenti+github@proton.me>

--- tests/launch.rs
+++ tests/launch.rs
@@ -7,7 +7,8 @@ use tdx::launch::TdxVm;
 #[test]
 fn launch() {
     let kvm_fd = Kvm::new().unwrap();
-    let tdx_vm = TdxVm::new(&kvm_fd).unwrap();
+    let tdx_vm = TdxVm::new(&kvm_fd, 100).unwrap();
     let caps = tdx_vm.get_capabilities().unwrap();
     let _ = tdx_vm.init_vm(&kvm_fd, &caps).unwrap();
+    let _vcpufd = tdx_vm.fd.create_vcpu(0).unwrap();
 }
