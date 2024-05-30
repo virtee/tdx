81c10e3527056cca289561421f335a4084a70487
Jake Correnti <jakecorrenti+github@proton.me>
2024-05-30 10:59:44 -0400

    provide TdxVcpu user API
    Signed-off-by: Jake Correnti <jakecorrenti+github@proton.me>

--- tests/launch.rs
+++ tests/launch.rs
@@ -2,12 +2,12 @@
 
 use kvm_ioctls::Kvm;
 
-use tdx::launch::TdxVm;
+use tdx::launch::{TdxVcpu, TdxVm};
 use tdx::tdvf;
 
 #[test]
 fn launch() {
-    let kvm_fd = Kvm::new().unwrap();
+    let mut kvm_fd = Kvm::new().unwrap();
 
     // create vm
     let tdx_vm = TdxVm::new(&kvm_fd, 100).unwrap();
@@ -15,8 +15,10 @@ fn launch() {
     let _ = tdx_vm.init_vm(&kvm_fd, &caps).unwrap();
 
     // create vcpu
-    let _vcpufd = tdx_vm.fd.create_vcpu(10).unwrap();
+    let mut vcpufd = tdx_vm.fd.create_vcpu(10).unwrap();
+    let tdx_vcpu = TdxVcpu::try_from((&mut vcpufd, &mut kvm_fd)).unwrap();
     let mut firmware = std::fs::File::open("./tests/data/OVMF.inteltdx.fd").unwrap();
     let sections = tdvf::parse_sections(&mut firmware).unwrap();
-    let _hob_section = tdvf::get_hob_section(&sections).unwrap();
+    let hob_section = tdvf::get_hob_section(&sections).unwrap();
+    tdx_vcpu.init(hob_section.memory_address).unwrap();
 }
