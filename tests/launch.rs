ee0d9a447ea64572b4da2af6f9ff1c335fc6f2a5
Jake Correnti <jakecorrenti+github@proton.me>
2024-05-30 10:57:34 -0400

    test tdvf
    Signed-off-by: Jake Correnti <jakecorrenti+github@proton.me>

--- tests/launch.rs
+++ tests/launch.rs
@@ -3,12 +3,20 @@
 use kvm_ioctls::Kvm;
 
 use tdx::launch::TdxVm;
+use tdx::tdvf;
 
 #[test]
 fn launch() {
     let kvm_fd = Kvm::new().unwrap();
+
+    // create vm
     let tdx_vm = TdxVm::new(&kvm_fd, 100).unwrap();
     let caps = tdx_vm.get_capabilities().unwrap();
     let _ = tdx_vm.init_vm(&kvm_fd, &caps).unwrap();
-    let _vcpufd = tdx_vm.fd.create_vcpu(0).unwrap();
+
+    // create vcpu
+    let _vcpufd = tdx_vm.fd.create_vcpu(10).unwrap();
+    let mut firmware = std::fs::File::open("./tests/data/OVMF.inteltdx.fd").unwrap();
+    let sections = tdvf::parse_sections(&mut firmware).unwrap();
+    let _hob_section = tdvf::get_hob_section(&sections).unwrap();
 }
