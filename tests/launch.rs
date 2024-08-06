d93bdf93a48f2de6c9a022fc006cda28ad3c88a3
Jake Correnti <jakecorrenti+github@proton.me>
2024-08-06 09:35:26 -0400

    Add mmap helper functions
    Adds `align_down` which rounds a number down to multiple. Equivalent to
    the `ALIGN_DOWN` macro in QEMU.
    
    Adds `align_up` which rounds a number up to multiple. Equivalent to the
    `ALIGN_UP` macro in QEMU`.
    
    Adds `mmap_reserve` which reserves a new memory region of the requested
    size to be used for mapping from the given fd (if any). This is
    equivalent to `mmap_reserve` in QEMU.
    
    Adds `mmap_activate` which activates memory in a reserved region from
    the given fd (if any), to make it accessible.  This is equivalent to
    `mmap_activate` in QEMU.
    
    Signed-off-by: Jake Correnti <jakecorrenti+github@proton.me>

--- tests/launch.rs
+++ tests/launch.rs
@@ -22,3 +22,56 @@ fn launch() {
     let hob_section = tdvf::get_hob_section(&sections).unwrap();
     tdx_vcpu.init(hob_section.memory_address).unwrap();
 }
+
+/// Round number down to multiple
+pub fn align_down(n: usize, m: usize) -> usize {
+    n / m * m
+}
+
+/// Round number up to multiple
+pub fn align_up(n: usize, m: usize) -> usize {
+    align_down(n + m - 1, m)
+}
+
+/// Reserve a new memory region of the requested size to be used for maping from the given fd (if
+/// any)
+pub fn mmap_reserve(size: usize, fd: i32) -> *mut libc::c_void {
+    let mut flags = libc::MAP_PRIVATE;
+    flags |= libc::MAP_ANONYMOUS;
+    unsafe { libc::mmap(0 as _, size, libc::PROT_NONE, flags, fd, 0) }
+}
+
+/// Activate memory in a reserved region from the given fd (if any), to make it accessible.
+pub fn mmap_activate(
+    ptr: *mut libc::c_void,
+    size: usize,
+    fd: i32,
+    map_flags: u32,
+    map_offset: i64,
+) -> *mut libc::c_void {
+    let noreserve = map_flags & (1 << 3);
+    let readonly = map_flags & (1 << 0);
+    let shared = map_flags & (1 << 1);
+    let sync = map_flags & (1 << 2);
+    let prot = libc::PROT_READ | (if readonly == 1 { 0 } else { libc::PROT_WRITE });
+    let mut map_synced_flags = 0;
+    let mut flags = libc::MAP_FIXED;
+
+    flags |= if fd == -1 { libc::MAP_ANONYMOUS } else { 0 };
+    flags |= if shared >= 1 {
+        libc::MAP_SHARED
+    } else {
+        libc::MAP_PRIVATE
+    };
+    flags |= if noreserve >= 1 {
+        libc::MAP_NORESERVE
+    } else {
+        0
+    };
+
+    if shared >= 1 && sync >= 1 {
+        map_synced_flags = libc::MAP_SYNC | libc::MAP_SHARED_VALIDATE;
+    }
+
+    unsafe { libc::mmap(ptr, size, prot, flags | map_synced_flags, fd, map_offset) }
+}
