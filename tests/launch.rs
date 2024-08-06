fac0895c90d7ad62330f0087adbdcf7a664e876e
Jake Correnti <jakecorrenti+github@proton.me>
2024-08-06 09:45:51 -0400

    Implement `init_mem_region` testing
    Extends the `tests/launch.rs` test to include `init_mem_region`.
    
    Based on the QEMU implementation, if the host supports the
    `KVM_CAP_MEMORY_MAPPING` extension, then we should use a different set
    of ioctls: `KVM_MEMORY_MAPPING` and `KVM_TDX_EXTEND_MEMORY` instead of
    `KVM_TDX_INIT_MEM_REGION`. However, the current state of the CentOS SIG
    build doesn't support these ioctls so I have no way to test.
    
    Signed-off-by: Jake Correnti <jakecorrenti+github@proton.me>
    
    underscore `caps` to silence warning
    
    Signed-off-by: Jake Correnti <jakecorrenti+github@proton.me>

--- tests/launch.rs
+++ tests/launch.rs
@@ -6,22 +6,103 @@ use vmm_sys_util::*;
 use tdx::launch::{TdxVcpu, TdxVm};
 use tdx::tdvf;
 
+// `mov eax,1000h` will set the value in the register eax (and rax since they both share the bottom 32 bits) to 1000h
+// `jmp *%rax` will jump the program to the address that rax contains, which in this case will be 1000h
+const FIRMWARE: &[u8; 7] = &[
+    0xb8, 0x00, 0x10, 0x00, 0x00, // mov eax, 1000h
+    0xff, 0xe0, // jmp *%rax
+];
+
 #[test]
 fn launch() {
-    let mut kvm_fd = Kvm::new().unwrap();
+    const KVM_CAP_GUEST_MEMFD: u32 = 234;
+    const KVM_CAP_MEMORY_MAPPING: u32 = 236;
 
     // create vm
+    let mut kvm_fd = Kvm::new().unwrap();
     let tdx_vm = TdxVm::new(&kvm_fd, 100).unwrap();
-    let caps = tdx_vm.get_capabilities().unwrap();
-    let _ = tdx_vm.init_vm(&kvm_fd, &caps).unwrap();
+    let _caps = tdx_vm.get_capabilities().unwrap();
+    let _ = tdx_vm.init_vm(&kvm_fd).unwrap();
+
+    // get tdvf sections
+    let mut firmware = std::fs::File::open("tests/data/OVMF.fd").unwrap();
+    let sections = tdvf::parse_sections(&mut firmware).unwrap();
+    let hob_section = tdvf::get_hob_section(&sections).unwrap();
 
     // create vcpu
     let mut vcpufd = tdx_vm.fd.create_vcpu(10).unwrap();
     let tdx_vcpu = TdxVcpu::try_from((&mut vcpufd, &mut kvm_fd)).unwrap();
-    let mut firmware = std::fs::File::open("./tests/data/OVMF.inteltdx.fd").unwrap();
-    let sections = tdvf::parse_sections(&mut firmware).unwrap();
-    let hob_section = tdvf::get_hob_section(&sections).unwrap();
     tdx_vcpu.init(hob_section.memory_address).unwrap();
+
+    // map memory to guest
+    if !check_extension(KVM_CAP_GUEST_MEMFD) {
+        panic!("KVM_CAP_GUEST_MEMFD isn't supported, which is required by TDX");
+    }
+
+    // In TDX you cannot modify the registers directly since they are
+    // confidential. Therefore, if you want the VM to run custom code,
+    // you need to map it to the reset vector on the guest: 0xfffffff0.
+
+    // Start with the first 4k of memory (4G - 4k) as all 0s.
+    let firmware_code = &mut [0u8; 4096].to_vec();
+
+    // Map the firmware we want the VM to run on boot to the reset
+    // vector, which is at 4G - 16B (0xfffffff0).
+    for (idx, b) in FIRMWARE.iter().enumerate() {
+        firmware_code[4096 - 16 + idx] = *b;
+    }
+
+    let firmware_userspace = ram_mmap(firmware_code.len() as u64);
+    // (4 << 30) - 0x1000
+    let guest_addr = 0xfffff000u64;
+
+    // copy the firmware code into the memory allocated for `firmware_userspace`
+    let address_space: &mut [u8] = unsafe {
+        std::slice::from_raw_parts_mut(firmware_userspace as *mut u8, firmware_code.len())
+    };
+    address_space[..firmware_code.len()].copy_from_slice(&firmware_code[..]);
+    let firmware_userspace = address_space as *const [u8] as *const u8 as u64;
+
+    let gmem = kvm_bindings::kvm_create_guest_memfd {
+        size: firmware_code.len() as u64,
+        flags: 0,
+        reserved: [0; 6],
+    };
+
+    let gmem = tdx_vm.fd.create_guest_memfd(gmem).unwrap();
+    let region = kvm_bindings::kvm_userspace_memory_region2 {
+        slot: 0 as u32,
+        // KVM_MEM_GUEST_MEMFD
+        flags: 1 << 2,
+        guest_phys_addr: guest_addr,
+        memory_size: firmware_code.len() as u64,
+        userspace_addr: firmware_userspace,
+        guest_memfd_offset: 0,
+        guest_memfd: gmem as u32,
+        pad1: 0,
+        pad2: [0; 14],
+    };
+    unsafe {
+        tdx_vm.fd.set_user_memory_region2(region).unwrap();
+    }
+
+    let attr = kvm_bindings::kvm_memory_attributes {
+        address: guest_addr,
+        size: firmware_code.len() as u64,
+        // KVM_MEMORY_ATTRIBUTE_PRIVATE
+        attributes: 1 << 3,
+        flags: 0,
+    };
+    tdx_vm.fd.set_memory_attributes(attr).unwrap();
+
+    if check_extension(KVM_CAP_MEMORY_MAPPING) {
+        // TODO(jakecorrenti): the current CentOS SIG doesn't support the KVM_MEMORY_MAPPING or
+        // KVM_TDX_EXTEND_MEMORY ioctls, which is what we would typically use here.
+    } else {
+        tdx_vm
+            .init_mem_region(guest_addr, 1, 1, firmware_userspace)
+            .unwrap();
+    }
 }
 
 /// Round number down to multiple
